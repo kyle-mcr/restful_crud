8bfa760294fd4c6d4d74e566fc9970208a5840a8
Kyle McReynolds <53325780+kyle-mcr@users.noreply.github.com>
2019-10-15 11:51:44 -0700

    Add files via upload
    MEAN stack Boiler Plate, restful api including CRUD routing.

---
+++ restful_task_api/public/src/app/show/show.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ThrowStmt } from '@angular/compiler';
+
+@Component({
+  selector: 'app-show',
+  templateUrl: './show.component.html',
+  styleUrls: ['./show.component.css']
+})
+export class ShowComponent implements OnInit {
+  myobj: any;
+  id: any;
+  constructor(
+    private http: HttpService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) { }
+  ngOnInit() {
+    this.getData();
+    this.myobj = { title: '', description: '', completed: false };
+  }
+  getTask(id) {
+    let observable2 = this.http.findOne(id);
+    observable2.subscribe(data => {
+      this.myobj = data;
+      console.log(this.myobj);
+    })
+  }
+
+  getData() {
+    let observable = this.route.params.subscribe(data => {
+      this.id = data['id'];
+      this.getTask(this.id);
+    })
+  }
+}
